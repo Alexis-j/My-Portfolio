c195125e9d43a698f1261b75ab0698b9c1374528
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-07-13 14:50:38 +0200

    images

--- src/components/body/MyProjects.jsx
+++ src/components/body/MyProjects.jsx
@@ -1,5 +1,7 @@
-import { Container, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import React from "react";
+import  Soozie  from "../../assets/img/Projecto Soozie.png";
+import  SunshineSage  from "../../assets/img/Projecto Wheater.png";
 
 export default function Myprofile() {
   return (
@@ -10,6 +12,7 @@ export default function Myprofile() {
       variant="h2"
       component="h4"
       align="center"
+      gutterBottom
       >
       My Projects
       </Typography>
@@ -17,11 +20,47 @@ export default function Myprofile() {
         variant="h4"
         component="h4"
         align="center"
+        gutterBottom
       >
       A selection of my current work
       </Typography>
+      <Box
+        sx={{
+          display:"columns",
+          justifyContent:"center",
+          alignItems:"center",
 
-
+        }}
+      >
+        <Box
+        sx={{ // esta es la imagen
+          width: 300,
+          height: 275,
+          boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+          borderRadius: "5px",
+          marginBottom: 0,
+          '@media (min-width: 600px)': {
+            marginBottom: 0,
+            marginRight: "1rem",
+          }
+        }}>
+          <img src={SunshineSage} alt="Projecto Wheater"style={{ width: "auto", height: '100%', objectFit: 'cover', borderRadius: "5px", display: "block", margin: 0}} />
+        </Box>
+        <Box
+        sx={{ // esta es la imagen
+          width: 300,
+          height: 275,
+          boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+          borderRadius: "5px",
+          marginBottom: "0",
+          '@media (min-width: 600px)': {
+            marginBottom: 0,
+            marginRight: "1rem",
+          }
+        }}>
+          <img src={Soozie} alt="Projecto Soozie" style={{ width: "auto", height: '100%', objectFit: 'cover', borderRadius: "5px", display: "block", margin: 0}}/>
+        </Box>
+      </Box>
     </Container>
   )
 }
