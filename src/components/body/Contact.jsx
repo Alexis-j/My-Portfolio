29dc1d6af1651b1a763dd22f0c21e65f78304d0c
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-07-21 19:26:02 +0200

    changes

---
+++ src/components/body/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import portraitImage from "../../assets/img/portrait.jpg"
+
+
+export default function Contact() {
+  return(
+    <>
+    <h1>hola</h1>
+    </>
+  )
+}
