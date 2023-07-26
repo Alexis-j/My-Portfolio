d9fffb21651c6aa0c21c05bae83677e6d02651e3
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-07-26 13:00:32 +0200

    final touches in about me

--- src/components/body/AboutMe.jsx
+++ src/components/body/AboutMe.jsx
@@ -11,8 +11,10 @@ export default function AboutMe() {
         display: "grid",
         gridTemplateColumns: "1fr", // Solo una columna en pantallas pequeñas
         gap: 2,
+        height: "100vh", // Esta línea hará que la sección ocupe toda la pantalla verticalmente
         '@media (min-width: 600px)': {
           gridTemplateColumns: "2fr 1fr", // 66% para el título, 33% para el contenido en pantallas grandes
+          gridTemplateRows: "auto 0.15fr 2fr auto", // Ajustar las filas para una mejor distribución vertical
         },
       }}
     >
@@ -30,16 +32,21 @@ export default function AboutMe() {
         About Me
       </Typography>
 
-      <Box
-        sx={{
-          bgcolor: "#084d01",
-          color:"#ffffff",
-          borderRadius:"2px",
-          textAlign: "start",
-          gridColumn: "1", // Toma toda la columna disponible
-        }}
-      >
-        <Typography style={{paddingLeft:10, fontSize:"24px", gridRow:"2"}}>
+          <Box
+          sx={{
+            bgcolor: "#084d01",
+            color: "#ffffff",
+            borderRadius: "2px",
+            textAlign: "start",
+            gridColumn: "1", // Toma toda la columna disponible
+            gridRow: "2", // Segunda fila en pantallas grandes
+            '@media (min-width: 600px)': {
+              gridColumn: "1", // Ocupa la primera columna en pantallas grandes
+              gridRow: "2 / 3", // Ocupa solo una tercera parte de la altura en pantallas grandes
+            },
+          }}
+        >
+        <Typography style={{paddingLeft:10, fontSize:"24px"}}>
           Full Stack Web Dev
         </Typography>
       </Box>
@@ -47,9 +54,11 @@ export default function AboutMe() {
       <Typography
         sx={{
           gridColumn: "1", // Toma toda la columna disponible
-          gridRow:"3"
+          gridRow: "3", // Tercera fila en pantallas grandes
         }}
       >
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum nemo cum nihil? Quibusdam nobis autem doloribus recusandae voluptates commodi libero ut laborum ullam, deleniti nam aperiam. Quod sapiente temporibus nemo.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore tempora adipisci amet exercitationem atque, quos ut maiores saepe voluptates sed non eligendi dicta. Non eligendi quidem assumenda esse iusto odit?Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quo modi, libero quisquam sequi distinctio amet rem similique! In animi repellat nostrum sunt ipsam id officiis delectus, commodi iste quam?
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis temporibus beatae assumenda ea dolorum delectus nesciunt esse, aliquid similique, vel sapiente numquam odit. Non a dolorum aliquam commodi est asperiores.
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, quas. Dicta accusamus, earum quia hic facilis suscipit! Quia, quis ipsam? Debitis consequatur illum est. Quidem id corrupti assumenda? Quidem, earum.
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, quibusdam! Explicabo facere maxime corporis at excepturi, aperiam illo ad iure odit, sed eaque nihil, nulla aliquid quibusdam omnis eius cumque!
@@ -59,13 +68,14 @@ export default function AboutMe() {
         sx={{
           // esta es la imagen
           width: "100%", // Toma todo el ancho disponible en pantallas pequeñas
-          height: 275,
+          height: 350,
           maxHeight: "100%",
           boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
           borderRadius: "5px",
           '@media (min-width: 600px)': {
             gridColumn: "2",
-            gridRow:"2 / 4" // Toma toda la primera columna en pantallas grandes
+            gridRow: "3 / 4", // Ocupa las filas 2 y 3 en pantallas grandes
+            minHeight: "275px", // Altura mínima para limitar el crecimiento del elemento
           },
         }}
       >
--- src/components/body/MyProjects.jsx
+++ src/components/body/MyProjects.jsx
@@ -4,62 +4,66 @@ import  SunshineSage  from "../../assets/img/Projecto Wheater.png";
 
 export default function MyProjects() {
   return (
-    <Container id="MyProjects" sx={{height:"100vh"}}
-    maxWidth="md"
-    >
-      <Typography
-      variant="h2"
-      component="h4"
-      align="center"
-      gutterBottom
+    <section  id="Projects" style={{ backgroundColor: "black", alignItems: "center" }}>
+      <Container id="MyProjects" sx={{padding:5 }}
+      maxWidth="md"
       >
-      My Projects
-      </Typography>
-      <Typography
-        variant="h4"
+        <Typography
+        variant="h2"
         component="h4"
         align="center"
         gutterBottom
-      >
-      A selection of my current work
-      </Typography>
-      <Box
-        sx={{
-          display:"columns",
-          justifyContent:"center",
-          alignItems:"center",
+        style={{color:"#ffffff"}}
+        >
+        My Projects
+        </Typography>
+        <Typography
+          variant="h5"
+          component="h4"
+          align="center"
+          gutterBottom
+          style={{color:"#ffffff"}}
+        >
+        A selection of my current work
+        </Typography>
+        <Box
+          sx={{
+            display:"columns",
+            justifyContent:"center",
+            alignItems:"center",
 
-        }}
-      >
-        <Box
-        sx={{ // esta es la imagen
-          width: 300,
-          height: 275,
-          boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
-          borderRadius: "5px",
-          marginBottom: 0,
-          '@media (min-width: 600px)': {
+          }}
+        >
+          <Box
+          sx={{ // esta es la imagen
+            width: 300,
+            height: 275,
+            boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+            borderRadius: "5px",
             marginBottom: 0,
-            marginRight: "1rem",
-          }
-        }}>
-          <img src={SunshineSage} alt="Projecto Wheater"style={{ width: "auto", height: '100%', objectFit: 'cover', borderRadius: "5px", display: "block", margin: 0}} />
+            '@media (min-width: 600px)': {
+              marginBottom: 0,
+              marginRight: "1rem",
+            }
+          }}>
+            <img src={SunshineSage} alt="Projecto Wheater"style={{ width: "auto", height: '100%', objectFit: 'cover', borderRadius: "5px", display: "block", margin: 0}} />
+          </Box>
+          <Box
+          sx={{ // esta es la imagen
+            width: 300,
+            height: 275,
+            boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+            borderRadius: "5px",
+            marginBottom: "0",
+            '@media (min-width: 600px)': {
+              marginBottom: 0,
+              marginRight: "1rem",
+            }
+          }}>
+            <img src={Soozie} alt="Projecto Soozie" style={{ width: "auto", height: '100%', objectFit: 'cover', borderRadius: "5px", display: "block", margin: 0}}/>
+          </Box>
         </Box>
-        <Box
-        sx={{ // esta es la imagen
-          width: 300,
-          height: 275,
-          boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
-          borderRadius: "5px",
-          marginBottom: "0",
-          '@media (min-width: 600px)': {
-            marginBottom: 0,
-            marginRight: "1rem",
-          }
-        }}>
-          <img src={Soozie} alt="Projecto Soozie" style={{ width: "auto", height: '100%', objectFit: 'cover', borderRadius: "5px", display: "block", margin: 0}}/>
-        </Box>
-      </Box>
-    </Container>
+      </Container>
+    </section>
   )
 }
