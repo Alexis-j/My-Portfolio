aa14e9fe639ea1151b5993d0f87c3f4421859003
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-09-05 18:57:22 +0200

    footer solve

--- src/components/body/Footer.jsx
+++ src/components/body/Footer.jsx
@@ -3,15 +3,12 @@ import { Container, Typography } from "@mui/material";
 export default function Footer() {
   return (
     <section id="footer">
-      <Container maxWidth="xl" sx={{ backgroundColor: "#001023", height: "50px", display: "flex", alignItems: "center", justifyContent:"center" }}>
-        <Typography
-          variant="p"
-          component="p"
-          color="#fadb85"
-        >
+      <div style={{ backgroundColor: "#001023", height: "50px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <Typography variant="body1" style={{ color: "#fadb85" }}>
           © 2023 Alexis Jiménez Castillo. All rights reserved.
         </Typography>
-      </Container>
+      </div>
     </section>
   );
 }
+
