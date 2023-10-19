2d3c67324aac45f0896f930f7b447fc9a859b0ca
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-10-19 11:06:09 +0200

    adding the dark mode

--- src/components/btns/BtnDarkMode.jsx
+++ src/components/btns/BtnDarkMode.jsx
@@ -1,40 +1,44 @@
-import { Button } from "@mui/material";
+import React, { useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 export default function DarkModeBtn() {
-  const CustomButton = styled(Button)({
-    backgroundColor: "inherit",
-    color: "#fff",
-    width: 100,
-    marginTop: 20,
-    border: "2px solid",
-    transition: 'transform 0.3s', // Ajusta la duración de la transición aquí
-    textTransform: "none",
-    '&:hover': {
-      backgroundColor: "#fadb85",
-      color:"#001023",
-      transform: 'scale(1.05)',
-      textTransform: "none", // Ajusta el valor de escala para controlar el crecimiento
+  // Estado para controlar el modo oscuro
+  const [darkMode, setDarkMode] = useState(false);
+
+  // Función para alternar el modo oscuro
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  // Configura el tema de Material-UI basado en el modo oscuro
+  const theme = createTheme({
+    palette: {
+      type: darkMode ? 'dark' : 'light', // Configura el tipo de paleta según el modo oscuro
     },
   });
-  
-  const darkModeToggle = document.getElementById('darkModeToggle');
-  const body = document.body;
-  const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
-  if (isDarkMode) {
-      body.classList.add('dark-mode');
-      darkModeToggle.checked = true;
-  }
-  darkModeToggle.addEventListener('change', () => {
-      if (darkModeToggle.checked) {
-          body.classList.add('dark-mode');
-          localStorage.setItem('darkMode', 'enabled');
-      } else {
-          body.classList.remove('dark-mode');
-          localStorage.setItem('darkMode', 'disabled');
-      }
-  });
 
-  return(
+  // Actualiza el estado del modo oscuro en el almacenamiento local y en el cuerpo del documento
+  useEffect(() => {
+    const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+    setDarkMode(isDarkMode);
+  }, []);
 
-  )
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('dark-mode');
+      localStorage.setItem('darkMode', 'enabled');
+    } else {
+      document.body.classList.remove('dark-mode');
+      localStorage.setItem('darkMode', 'disabled');
+    }
+  }, [darkMode]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Button onClick={toggleDarkMode}>
+        {darkMode ? 'Desactivar Modo Oscuro' : 'Activar Modo Oscuro'}
+      </Button>
+    </ThemeProvider>
+  );
 }
