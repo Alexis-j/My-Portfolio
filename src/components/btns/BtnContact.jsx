d0b78e9b3fd8af189e7e4d6d6f5dea01e239cfdf
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-10-20 19:29:49 +0200

    contact btn

--- src/components/btns/BtnContact.jsx
+++ src/components/btns/BtnContact.jsx
@@ -1,28 +1,16 @@
 import { Button } from "@mui/material";
 
 export default function BtnContact() {
-  const handleButtonClick = (event) => {
-    event.preventDefault();
-    const targetElement = document.querySelector("#Contact"); // Reemplaza "#Contact" con el identificador de tu sección de contacto
-    if (targetElement) {
-      const topOffset = targetElement.getBoundingClientRect().top;
-      const headerOffset = 100;
-      const totalOffset = topOffset + window.pageYOffset - headerOffset;
+  const email = "mailto:alexis.jcastillo@outlook.com";
 
-      // Desplazamiento suave con retraso
-      setTimeout(() => {
-        window.scroll({
-          top: totalOffset,
-          behavior: "smooth",
-        });
-      }, 200); // Ajusta el tiempo de retraso aquí
-    }
+  const handleButtonClick = () => {
+    window.location.href = email;
   };
 
   return (
     <Button
       variant="contained"
-      onClick={handleButtonClick} // Utiliza el nuevo manejador de clic
+      onClick={handleButtonClick}
       style={{ backgroundColor: "#fadb85", color: "#001023", width: 150, marginTop: 20 }}
     >
       CONTACT
