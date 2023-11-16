e2d661978e6f7c160c2bc35889bd2b24998da6d7
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-11-16 11:59:18 +0100

    responsive year update

--- src/components/body/Footer.jsx
+++ src/components/body/Footer.jsx
@@ -1,11 +1,19 @@
 import { Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
 
 export default function Footer() {
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+
+  useEffect(() => {
+    // Actualizar el año actual
+    setCurrentYear(new Date().getFullYear());
+  }, []);
+
   return (
     <section id="footer">
       <div style={{ backgroundColor: "#001023", height: "50px", display: "flex", alignItems: "center", justifyContent: "center" }}>
         <Typography variant="body1" style={{ color: "#fadb85" }}>
-          © 2023 Alexis Jiménez Castillo. All rights reserved.
+          &copy; {currentYear} Alexis Jiménez Castillo. All rights reserved.
         </Typography>
       </div>
     </section>
