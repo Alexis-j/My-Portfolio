dda8ee95fbfc77d15308b58fec797bb83e3dd2f5
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2024-01-03 11:59:38 +0100

    small change in the navlistdrawer

--- src/components/Header/NavListDrawer.jsx
+++ src/components/Header/NavListDrawer.jsx
@@ -1,40 +1,28 @@
-import PropTypes from "prop-types";
-import { List, ListItem, ListItemText, ListItemButton, IconButton } from "@mui/material";
+import { List, ListItem, ListItemText, ListItemButton, IconButton, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import CloseIcon from '@mui/icons-material/Close';
 
 export default function NavListDrawer({ navLinks, NavLink, handleNavLinkClick, setOpen }) {
   return (
-    <Box sx={{ bgcolor: "#ffffff", display: "flex", justifyContent: "flex-end"}}>
-      <nav>
-        <List>
-          <Box sx={{ display: "flex", justifyContent: "flex-end", alignItems: "center", padding: "0 16px" }}>
-            <IconButton onClick={() => setOpen(false)}>
-              <CloseIcon />
-            </IconButton>
-          </Box >
-          {navLinks.map((item) => (
-            <ListItem disablePadding key={item.title}>
-              <ListItemButton
-                component={NavLink}
-                to={item.path}
-                onClick={(event) => handleNavLinkClick(event, item.path)}
-              >
-                <ListItemText>{item.title}</ListItemText>
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </nav>
+    <Box sx={{ bgcolor: "#ffffff", display: "flex", flexDirection: "column", alignItems: "center"}}>
+      <IconButton sx={{ alignSelf: "flex-end", ml: "8px", mt: "8px", color:"black" }} onClick={() => setOpen(false)}>
+        <CloseIcon />
+      </IconButton>
+      <List sx={{ width: "100%" }}>
+        {navLinks.map((item) => (
+          <ListItem disablePadding key={item.title}>
+            <ListItemButton
+              component={NavLink}
+              to={item.path}
+              onClick={(event) => handleNavLinkClick(event, item.path)}
+            >
+              <ListItemText>
+                <Typography sx={{ textAlign: "center", fontWeight: "500" }}>{item.title}</Typography>
+              </ListItemText>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </Box>
   );
-}
-
-NavListDrawer.propTypes = {
-  navLinks: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      path: PropTypes.string.isRequired,
-    })
-  )
 }
