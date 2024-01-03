288fe7a5e7c1b020accc03f13f9f87b6a45a6489
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2024-01-03 12:20:43 +0100

    small change in the navlistdrawer

--- src/components/Header/NavListDrawer.jsx
+++ src/components/Header/NavListDrawer.jsx
@@ -1,8 +1,24 @@
 import { List, ListItem, ListItemText, ListItemButton, IconButton, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import CloseIcon from '@mui/icons-material/Close';
+import { useEffect } from "react";
 
 export default function NavListDrawer({ navLinks, NavLink, handleNavLinkClick, setOpen }) {
+  useEffect(() => {
+    function handleResize() {
+      // Cerrar el drawer cuando cambia el tamaño de la pantalla
+      if (window.innerWidth > 768) { // Define aquí el ancho límite para cerrar el drawer
+        setOpen(false);
+      }
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [setOpen]);
+
   return (
     <Box sx={{ bgcolor: "#ffffff", display: "flex", flexDirection: "column", alignItems: "center"}}>
       <IconButton sx={{ alignSelf: "flex-end", ml: "8px", mt: "8px", color:"black" }} onClick={() => setOpen(false)}>
